f552fb9a31ae42c06f993413663330a7e8f153c4
Pablo Mella <pablo.mella.r@gmail.com>
2019-10-09 00:00:17 -0400

    fixing network name for GKE

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -13,13 +13,14 @@ module "gke" {
   project_id = "${var.project}"
   name       = "${local.cluster_type}-cluster${var.cluster_name_suffix}"
   region     = "${var.region}"
-  network    = "${var.network}"
+  network    = "${var.env}"
   subnetwork = "${var.subnetwork}"
 
   ip_range_pods          = "${var.ip_range_pods}"
   ip_range_services      = "${var.ip_range_services}"
   create_service_account = false
   service_account        = "${var.compute_engine_service_account}"
+
 }
 
 resource "kubernetes_pod" "nginx-example" {
